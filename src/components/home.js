bc3e4d674a3a9390181d09bcec38ffd6faaf7372
benjamin kisenge <benkisenge03@gmail.com>
2022-09-14 20:37:13 +0200

    Fix eslint error

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,5 @@
-import Book from "./book";
+import Book from './book';
+import CreateBook from './createNewBook';
 
 const Home = () => (
   <div className="home">
@@ -9,18 +10,7 @@ const Home = () => (
         </select>
       </div>
       <Book />
-      <div className="add-book">
-        <h2>Add new book</h2>
-        <form>
-          <input type="text" className="book-title" placeholder="Book title" />
-          <div className="selector">
-            <select>
-              <option value="0">Category</option>
-            </select>
-          </div>
-          <button type="submit">Add book</button>
-        </form>
-      </div>
+      <CreateBook />
     </div>
   </div>
 );
