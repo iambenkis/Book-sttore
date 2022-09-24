a3974883890dfa5a1d4d419f0495734ccfb9032e
benjamin kisenge <benkisenge03@gmail.com>
2022-09-24 09:58:53 +0200

    Create service function

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,4 +1,7 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import store from "../configureStore";
+
+const APIURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/1fOnn2C5yMKtspS4K1tx/books';
 
 export const removeBook = (index) => ({
   type: 'REMOVE',
@@ -10,7 +13,11 @@ export const addBook = (book) => ({
   payload: book,
 });
 
-const simpleBook = books => ({type: 'LOGIN',books})
+export const read = (books) => ({
+  type: 'READ_BOOK',
+  payload: books,
+});
+
 
 const thunkBook = () => () =>
   axios.get('./api/auh/me')
@@ -37,9 +44,33 @@ const bookReducer = (state = initialState, action) => {
       return [...state.filter((book) => book.id !== action.payload)];
     case 'ADD_BOOK':
       return [...state, action.payload];
+    case 'READ_BOOK':
+        return action.payload;
     default:
       return state;
   }
 };
+
+export const recieveBooks = createAsyncThunk(READ,
+  async () => {
+    const res = await axios.get(APIURL);
+    const books = Object.keys(res.data).map((id) => ({
+      item_id: id,
+      ...res.data[id][0],
+    }));
+    return books;
+  });
+
+export const sendBook = createAsyncThunk(ADD,
+  async (book) => {
+    await axios.post(APIURL, book);
+    return book;
+  });
+
+export const deleteBook = createAsyncThunk(REMOVE,
+  async (id) => {
+    await axios.delete(`${APIURL}/${id}`);
+    return id;
+  });
 
 export default bookReducer;
